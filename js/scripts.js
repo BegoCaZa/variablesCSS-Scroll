e711c7d4747477f356458aa84fc66b2c3c969ec4
BegoCaZa <begoxcz@hotmail.com>
2025-04-08 11:18:19 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,76 @@
 const rootStyles = document.documentElement.style;
-const titleElement = document.getElementById("title");
-const scrollElement=document.getElementsByClassName(".scroll-line");
-const backgroundColor=document.getElementsByClassName('--background-color');
-const totalWindowScroll=document.body.scrollHeight;
-const rgbButtonElement = document.getElementById("rgb-button");
-const hexaButtonElement = document.getElementById("hexa-button");
-
+const titleElement = document.getElementById('title');
+const totalWindowScroll = document.body.scrollHeight;
+const rgbButtonElement = document.getElementById('rgb-button');
+const hexaButtonElement = document.getElementById('hexa-button');
 
 //cosas que necesito pero no se para que
 // Scroll total de la ventana: document.body.scrollHeight
 // Alto de la ventana: window.innerHeight
 // Cantidad de scroll: window.scrollY
 
-
-
 const changeWidthScroll = () => {
-    const scrolledContent= totalWindowScroll- window.scrollY;
-    //resto el total de la ventana menos el alto de la ventana
-    
-    //cambiar variable css
-    rootStyles.setProperty("--width-scroll", scrolledContent);
-    
-    //actualizo el texto del elemento
-    titleElement.textContent= `Te has desplazado: ${scrolledContent} px`;
+  const scrolledPixels = window.scrollY; //lo que me muevo en la ventana
 
-    console.log(scrolledContent);
-}
-//EVENTS
-window.addEventListener('scroll', changeWidthScroll);
+  //regla de tres
+  const scrolledPercentage = Math.floor(
+    (window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight)
+  );
+
+  console.log(scrolledPercentage);
+
+  //cambiar variable css
+
+  rootStyles.setProperty('--width-scrollBar', `${scrolledPercentage}%`);
+
+  //actualizo el texto del elemento
+  titleElement.textContent = `Te has desplazado: ${scrolledPixels} px`;
+
+  //   console.log(scrolledPixels);
+};
 
 //FUNCIONES DE LOS BOTONES
 
-const generateRGBcolor=() => {
+const generateRGBcolor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  const newRGBcolor= Number(r,g,b); 
+  let newRGBcolor = `rgb(${r},${g},${b})`;
   console.log(newRGBcolor);
   return newRGBcolor;
-  
-}
+
+  // color: rgb(red, green, blue);
+};
 const changeRGBcolor = () => {
+  const newRGBcolor = generateRGBcolor();
+  rootStyles.setProperty('--background-color', newRGBcolor); //editr el css
+};
 
-    const newRGBcolor= generateRGBcolor();
-    rootStyles.setProperty("--background-color",newRGBcolor); //editr el css
+const generateHEXAcolor = () => {
+  let characters = '0123456789ABCDEF'; // posibles caracteres en un color hex
+  let colorHEXA = '#'; // que lleve #al principio
 
-}
+  for (let i = 0; i < 6; i++) {
+    randomIndex = Math.floor(Math.random() * characters.length);
+    colorHEXA += characters.charAt(randomIndex);
+  }
+  console.log(colorHEXA);
+  return colorHEXA;
 
-const generateHEXAcolor=() => {
-    let characters = '0123456789ABCDEF'; // posibles caracteres en un color hex
-    let colorHEXA = '#'; // que lleve #al principio
-
-    for (let i = 0; i < 6; i++) { 
-        randomIndex = Math.floor(Math.random() * characters.length); 
-        colorHEXA += characters.charAt(randomIndex); 
-    }
-    console.log(colorHEXA);
-    return colorHEXA;
-
-    //WIIII SI FUNCIONA
-}
+  //WIIII SI FUNCIONA
+};
 const changeHEXAcolor = () => {
+  const newHEXAcolor = generateHEXAcolor();
+  rootStyles.setProperty('--background-color', newHEXAcolor); //editr el css
+};
 
-    const newHEXAcolor= generateHEXAcolor();
-    rootStyles.setProperty("--background-color",newHEXAcolor); //editr el css
+const getMousePosition = event => {
+  rootStyles.setProperty('--mousePosition-X', `${event.clientY}px`);
+  rootStyles.setProperty('--mousePosition-Y', `${event.clientX}px`);
+};
 
-}
+//EVENTS
+window.addEventListener('scroll', changeWidthScroll);
 rgbButtonElement.addEventListener('click', changeRGBcolor);
 hexaButtonElement.addEventListener('click', changeHEXAcolor);
-
+document.addEventListener('mousemove', getMousePosition);
